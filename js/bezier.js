76d690abd5989aff7278c67ba6b270093c00979b
Daniel Christo <danielchristo@gmail.com>
2015-04-25 16:03:15 -0400

    General Refactoring and Fixing

---
+++ js/bezier.js
@@ -0,0 +1,134 @@
+function drawSoundwave(canvas) {
+	   								
+				if (this.strokeWidth > 0) {
+					canvas.strokeStyle = this.strokeColor;
+					canvas.lineWidth = this.strokeWidth;
+				}
+				
+				// Draw red control lines for every other two control points if there's no lock
+				canvas.beginPath();
+		 		if (this.control == 1) {canvas.strokeStyle = "#000000";} else {canvas.strokeStyle = "#FFF";}
+		 		for (i = 0; i < this.points.length; i++){this.points[i].strokeColor = canvas.strokeStyle;}
+
+		 		for (i = 2; i < this.points.length; i+=2)
+				{
+					if (this.control == 1) {
+						canvas.moveTo(this.points[i-1].x, this.points[i-1].y);
+						canvas.lineTo(this.points[i].x, this.points[i].y);
+					}
+
+				}
+				canvas.stroke();
+	 			canvas.closePath();	
+	 			
+				c = bezierCurvePath(this.points, $("#points").val(), canvas.canvas.width );
+				
+				
+				canvas.beginPath();
+				canvas.strokeStyle = "#000000";
+				for (t = 0; t < c.length ; t++) {
+					canvas.fillRect(c[t].x,c[t].y,1,1);
+				}  
+				canvas.stroke();
+		 		canvas.closePath();
+}
+
+
+
+// Custom bezier drawing function for this specific application.
+// Computes an array of equidistant dots along a bezier curve
+//  
+// ctrlPointArray: An array of bezier curve control coordinates
+// numDots: The number of dots
+function bezierCurvePath(ctrlPointArray, numDots, canvasLength) {
+	
+	var bezierCoordArray = [];
+
+	// At least four points are necessary for a bezier
+	if (ctrlPointArray.length > 3) {
+		
+		// In order to smoothly curve from the end into the beginning
+		// Set the first point's x value to be the same distance from the beginning as the last point is from the end
+		ctrlPointArray[0].x = canvasLength - ctrlPointArray[ctrlPointArray.length-1].x
+		
+		// The first curve is a special case where it starts at the midpoint between the last control point and the first control point
+		var startx = 0;
+		var starty = (ctrlPointArray[0].y + ctrlPointArray[ctrlPointArray.length-1].y) / 2
+ 		// and the last point is the midpoint between point 3 and point 4
+		var midx = (ctrlPointArray[1].x + ctrlPointArray[2].x) / 2
+		var midy = (ctrlPointArray[1].y + ctrlPointArray[2].y) / 2
+		var n = numDots / (ctrlPointArray.length / 4);
+		for (t = 0; t <= 1; t+=(1/n)) {
+			x = Math.pow(1 - t, 3)*startx + 3*Math.pow(1 -  t,2)*t*ctrlPointArray[0].x + 3*(1 -  t)*Math.pow(t,2)*ctrlPointArray[1].x + Math.pow(t,3)*midx;
+			y = Math.pow(1 - t, 3)*starty + 3*Math.pow(1 -  t,2)*t*ctrlPointArray[0].y + 3*(1 -  t)*Math.pow(t,2)*ctrlPointArray[1].y + Math.pow(t,3)*midy;
+			bezierCoordArray[bezierCoordArray.length] = {x:x, y:y};
+		}  
+		
+		var lastmidx;
+		var lastmidy;
+		// the remainder of the curves are drawn from from prior curves midpoint to the next curves midpoint thus only using three points
+		for (i = 2; i < ctrlPointArray.length - 3; i+=2)
+		{
+			lastmidx = midx;
+			lastmidy = midy;
+			var midx = (ctrlPointArray[i+1].x + ctrlPointArray[i+2].x) / 2
+			var midy = (ctrlPointArray[i+1].y + ctrlPointArray[i+2].y) / 2
+			var n = numDots / (ctrlPointArray.length / 4);
+			
+			for (t = 0; t <= 1; t+=(1/n)) {
+				x = Math.pow(1 - t, 3)*lastmidx + 3*Math.pow(1 -  t,2)*t*ctrlPointArray[i].x + 3*(1 -  t)*Math.pow(t,2)*ctrlPointArray[i+1].x + Math.pow(t,3)*midx;
+				y = Math.pow(1 - t, 3)*lastmidy + 3*Math.pow(1 -  t,2)*t*ctrlPointArray[i].y + 3*(1 -  t)*Math.pow(t,2)*ctrlPointArray[i+1].y + Math.pow(t,3)*midy;
+				bezierCoordArray[bezierCoordArray.length] = {x:x, y:y};
+			}  
+		}
+		
+		// The last curve ends at a midpoint between the last control point and the first control point
+		lastmidx = midx;
+		lastmidy = midy;
+		var endx = canvasLength
+		var endy = (ctrlPointArray[ctrlPointArray.length-1].y + ctrlPointArray[0].y) / 2
+		var n = numDots / (ctrlPointArray.length / 4);
+		
+		for (t = 0; t <= 1; t+=(1/n)) {
+			x = Math.pow(1 - t, 3)*lastmidx + 3*Math.pow(1 -  t,2)*t*ctrlPointArray[ctrlPointArray.length-2].x + 3*(1 -  t)*Math.pow(t,2)*ctrlPointArray[ctrlPointArray.length-1].x + Math.pow(t,3)*endx;
+			y = Math.pow(1 - t, 3)*lastmidy + 3*Math.pow(1 -  t,2)*t*ctrlPointArray[ctrlPointArray.length-2].y + 3*(1 -  t)*Math.pow(t,2)*ctrlPointArray[ctrlPointArray.length-1].y + Math.pow(t,3)*endy;
+			bezierCoordArray[bezierCoordArray.length] = {x:x, y:y};
+		} 
+	}
+	
+	// Once I have all the points for the bezier curve I need to loop through all of them looking for evenly spaced forward moving values of x
+	
+	// I should have the whole canvas for values of x
+	var len = canvasLength
+	var incr = len / numDots;
+	var j = 0;
+	
+	var evenSpacedArray = [];
+	var i = 1;
+	while ( i < bezierCoordArray.length) {
+		// if the smaller of the adjacent points x values is equal to j for then store it
+		if(j == bezierCoordArray[i-1].x) { 
+			evenSpacedArray[evenSpacedArray.length] = {x:bezierCoordArray[i-1].x, y:bezierCoordArray[i-1].y}; 
+			j+=incr; 
+		}
+		// if j is between the two points then find j along the line between the two points and store the y value
+		else if(j <= bezierCoordArray[i].x  && j > bezierCoordArray[i-1] < j) {
+			// compute the slope
+			var m = (bezierCoordArray[i].y - bezierCoordArray[i-1].y) / (bezierCoordArray[i].x - bezierCoordArray[i-1].x)
+			// compute the value of y at x = j
+			var y = m*(j - bezierCoordArray[i-1].x) + bezierCoordArray[i-1].y
+			// store the point
+			evenSpacedArray[evenSpacedArray.length] = {x:j, y:y};
+			j+=incr; 
+		}
+		
+		// if j is smaller than the first points x value then increment j
+		else if (j < bezierCoordArray[i-1].x) { j+=incr; }
+		// if j is larger than the last points x value then increment i
+		else if (j > bezierCoordArray[i].x) { i++; }
+			
+	}
+	  
+	return evenSpacedArray;
+
+}
---
+++ js/soundwave.js
@@ -0,0 +1,140 @@
+$(document).ready(
+
+	function(){ 
+			
+			// Create a new oCanvas object
+			var canvas = oCanvas.create({canvas: "#profileCanvas"});
+			canvas.width  = window.innerWidth;
+			
+			// Global variables for the Sound Wave object and its coordinates
+			// to be manipulated by button clicks and user events
+			var waveCoordCntr = 0;
+			var waveCoordArray = []; 
+			var soundWaveObj;
+			
+			// Global audio context to use in conjunction with the Sound Wave object to make a sound
+			var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+			
+			// Register a new soundWave shape on our oCanvas and link it to the drawSoundwave function for rendering
+			canvas.display.register("soundWave", {shapeType: "soundWave", control: 1}, drawSoundwave);
+
+			// Double clicking adds a control point
+			canvas.bind("dblclick", function (x) {
+			        
+			        // Right button press
+				if (x.which == 1) {
+					// Detect if button press is within an existing control point
+					var detectbit = 0;
+					if (waveCoordCntr > 0) {
+						for (i = 0; i < waveCoordArray.length; i++)
+						{
+							if ( Math.pow( canvas.mouse.x - waveCoordArray[i].x, 2) + Math.pow( canvas.mouse.y - waveCoordArray[i].y, 2) < Math.pow(waveCoordArray[i].radius,2)) {
+								detectbit = 1;
+							}
+						}
+					}
+					
+					// If not in an existing control point then add a new control point to the soundWave object
+					if (detectbit == 0) {
+						waveCoordCntr++;
+					
+		    			var ctrlPoint = canvas.display.ellipse({x: canvas.mouse.x, y: canvas.mouse.y, radius: 5,stroke: "1px #FF0000"});
+		    			waveCoordArray[waveCoordArray.length] = ctrlPoint;
+						canvas.addChild(ctrlPoint);
+						var dragOptions = { changeZindex: false };
+						ctrlPoint.dragAndDrop(dragOptions);
+						
+						// if this is the first control point create the soundWave object and add it to the canvas
+						if (waveCoordCntr == 1) {
+							soundWaveObj = canvas.display.soundWave({points: waveCoordArray, stroke: "1px #000", control: 1});
+							canvas.addChild(soundWaveObj);
+						}
+					}
+					
+				}
+				
+
+			});
+			
+			
+			canvas.bind("keydown", function (x) {
+			
+				// Spacebar turns on and off the control points and lines of the bezier object
+				if (x.which == 32) {
+					if(soundWaveObj.control == 0) {
+						soundWaveObj.control = 1;
+						canvas.redraw();
+					} else {
+						soundWaveObj.control = 0; 
+						canvas.redraw();
+					}
+				}
+				
+				// the letter E creates an endpoint
+				if (x.which == 69) {
+					var point = canvas.display.ellipse({x: canvas.width, y: canvas.height / 2, radius: 5,stroke: "1px #FF0000"});
+		    		waveCoordArray[waveCoordArray.length] = point;
+					canvas.addChild(point);
+				}
+				
+				// the letter C clears the canvas and resets the global variables
+				if (x.which == 67) {
+					canvas.children = [];
+					canvas.clear();
+					waveCoordCntr = 0;
+					waveCoordArray = [];
+					soundWaveObj = null;
+				}
+			
+			});
+			
+			$( '#play' ).click ( function () {
+			  
+			    var wave = [];
+			    wave = bezierCurvePath(waveCoordArray, $("#points").val(), canvas.width);
+			        
+			    // Stereo
+				var channels = 2;
+				
+				// Create an empty duration seconds stereo buffer at the
+				// sample rate of the AudioContext
+				var frameCount = audioCtx.sampleRate * $("#duration").val();
+				
+				var myArrayBuffer = audioCtx.createBuffer(channels, frameCount, audioCtx.sampleRate);
+
+				// Fill the buffer with my invented sound 
+				for (var channel = 0; channel < channels; channel++) {
+					// This gives us the actual array that contains the data
+					var nowBuffering = myArrayBuffer.getChannelData(channel);
+
+					// loop over the entire frame count
+					for (var j = 0; j < Math.floor(frameCount / wave.length); j++) {
+						for (var i = 0; i < wave.length; i++) {
+							nowBuffering[(j* wave.length) + i] = ((wave[i].y - (canvas.height / 2)) / canvas.height); 
+						}	
+					}
+					
+				}
+				
+				// Get an AudioBufferSourceNode
+				// This is the AudioNode to use when we want to play an AudioBuffer
+				var source = audioCtx.createBufferSource();
+				
+				// set the buffer in the AudioBufferSourceNode
+				source.buffer = myArrayBuffer;
+				
+				// connect the AudioBufferSourceNode to the
+				// destination so we can hear the sound
+				source.connect(audioCtx.destination);
+				
+				// start the source playing
+				source.start();
+
+
+			});
+	
+
+	
+	});
+	
