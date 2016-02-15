0f0ab828067fe3f4665bdb4063f75a114bd82d1f
Daniel Christo <danielchristo@gmail.com>
2016-02-15 18:02:54 -0500

    playing with variable visibility

--- js/soundwavelib/soundwave.js
+++ js/soundwavelib/soundwave.js
@@ -150,7 +150,7 @@ SoundWave.prototype.drawSoundwave = function(canvas) {
   }
 
   SoundWave.prototype.loadSoundwave = function(filename) {
-    var context = new(window.AudioContext || window.webkitAudioContext)();;
+    this.audioCtx = new(window.AudioContext || window.webkitAudioContext)();;
 
     bufferLoader = new BufferLoader(
       context,
@@ -165,7 +165,7 @@ SoundWave.prototype.drawSoundwave = function(canvas) {
 
   SoundWave.prototype.processLoadedSound = function(bufferList) {
     // Create two sources and play them both together.
-    var source1 = context.createBufferSource();
+    var source1 = this.audioCtx.createBufferSource();
     source1.buffer = bufferList[0];
     source1.connect(context.destination);
     source1.start(0);
